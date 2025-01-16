68baae94000d2cd438e46cb41786b4cec9bdcacf
Raeanne <raeannezou@gmail.com>
2025-01-16 11:15:03 +0800

    Added the getter and setter function to Actor.cpp and Movie.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -1,3 +1,17 @@
 #include "Actor.h"
 
 Actor::Actor(string name, int birthYear) : name(name), birthYear(birthYear) {}
+
+string Actor::getName() {
+	return name;
+}
+void Actor::setName(string name) {
+	this->name = name;
+}
+
+int Actor::getBirthYear() {
+	return birthYear;
+}
+void Actor::setBirthYear(int year) {
+	this->birthYear = year;
+}
--- Movie.cpp
+++ Movie.cpp
@@ -1,3 +1,27 @@
 #include "Movie.h"
 
 Movie::Movie(string title, string plot, int releaseYear) : title(title), plot(plot), releaseYear(releaseYear) {}
+
+string Movie::getTitle() {
+	return title;
+}
+
+void Movie::setTitle(string title) {
+	this->title = title;
+}
+
+string Movie::getPlot() {
+	return plot;
+}
+
+void Movie::setPlot(string plot) {
+	this->plot = plot;
+}
+
+int Movie::getReleaseYear() {
+	return releaseYear;
+}
+
+void Movie::setReleaseYear(int year) {
+	this->releaseYear = year;
+}
