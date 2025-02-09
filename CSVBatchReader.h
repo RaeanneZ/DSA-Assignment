2701805e6f8baaaaa4fa698cddd53f7f60400d47
Raeanne <raeannezou@gmail.com>
2025-02-09 13:37:21 +0800

    Added naming conventions on all files

--- CSVBatchReader.h
+++ CSVBatchReader.h
@@ -1,3 +1,12 @@
+/*
+* Member 1 Name: Zou Ruining, Raeanne
+* Member 1 ID: S10258772G
+* Member 2 Name: Cing Sian Kim
+* Member 2 ID: S10257716F
+* Group: 1
+*/
+
+
 #ifndef CSVBATCHREADER_H
 #define CSVBATCHREADER_H
 
--- User.cpp
+++ User.cpp
@@ -1,3 +1,11 @@
+/*
+* Member 1 Name: Zou Ruining, Raeanne
+* Member 1 ID: S10258772G
+* Member 2 Name: Cing Sian Kim
+* Member 2 ID: S10257716F
+* Group: 1
+*/
+
 #include "User.h"
 #include <iostream>
 
